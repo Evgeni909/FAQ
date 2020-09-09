74f7d8d0a753cdf03263aaa3ed0fd1de9840ce93
Evgeni <zorineevgeni94@gmail.com>
2020-09-10 01:23:07 +0300

    the project is almost done

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 $(function() {
     $('.toggle-button').click(function() {
-        $('.text').slideToggle();
+        $('.questions__icon-content').slideToggle();
     });
 });
+
+let element = document.querySelectorAll('.toggle');
+let element2 = document.querySelectorAll('.ver');
+let element3 = document.querySelectorAll('.hor');
+
+for (let i = 0; i < element.length; i++) {
+    element[i].onclick = () => {
+        element2[i].classList.toggle('active-ver');
+        element3[i].classList.toggle('active-hor');
+    };
+}
+
+
+
+
